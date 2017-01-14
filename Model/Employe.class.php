a4443dd18d1c86b180e7e7d4aad8d358092ba905
Aslamin Mohamed <maslamin@outlook.fr>
2017-01-14 12:58:25 +0100

    Employe model init

--- Model/Employe.class.php
+++ Model/Employe.class.php
@@ -1,6 +1,26 @@
 <?php
 class Employe{
-	
-	
+
+	private $loginEmp;
+	private $passwordEmp;
+	private $nomEmp;
+	private $prenomEmp;
+	private $dateEmbaucheEmp;
+	private $telEmp;
+	private $numService;
+	private $numTypeLogin;
+
+	funcion __construct($aLoginEmp, $aPasswordEmp, $aNomEmp, $aPrenomEmp, $aDateEmbaucheEmp, $aTelEmp, $aNumService, $aNumTypeLogin){
+
+		$this->$loginEmp         = $aLoginEmp;
+		$this->$passwordEmp      = $aPasswordEmp;
+		$this->$nomEmp           = $aNomEmp;
+		$this->$prenomEmp        = $aPrenomEmp;
+		$this->$dateEmbaucheEmp  = $aDateEmbaucheEmp;
+		$this->$telEmp           = $aTelEmp;
+		$this->$numService       = $aNumService;
+		$this->$numTypeLogin     = $aNumTypeLogin;
+
+	}
 }
 ?>
