b3148773304a3513acab4a603a63f377dc6b0d1e
Nicolas <nicolas.ramassamy.nr@gmail.com>
2017-01-14 12:14:32 +0100

    ClassConnectionDB

--- Database/ConnectionDB.class.php
+++ Database/ConnectionDB.class.php
@@ -1,8 +1,15 @@
 <?php
 
 public class ConnectionDB{
-	
+	private $HOST = "localhost";
+	private $HOSTNAME ="root";
+	private $PASSWORD = "";
+	private $DBNAME = "SIOA_TP_PRJ1";
+	private $conn;
 
-}
+	fonction __construct() {
+		$conn=mysqli_connect($this->$HOST,$this->HOSTNAME,$this->$PASSWORD,$this->$DBNAME);
+	}
+
 
 ?>
